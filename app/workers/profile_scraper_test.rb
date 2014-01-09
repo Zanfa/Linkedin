82fb16bb6f58cd7fd41bb0cd1455f6b92a7ba28a
Vahur Roosimaa <vahur.roosimaa@gmail.com>
2014-01-09 21:03:05 +0000

    Removed unnecessary test

--- app/workers/profile_scraper_test.rb
+++
@@ -1,7 +0,0 @@
-require './profile_scraper'
-require 'nokogiri'
-require 'open-uri'
-
-scraper = ProfileScraper.new
-result = scraper.find('James', 'Berdigans', 'Founder & CEO at ID-INFINITE')
-puts result
